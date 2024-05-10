32f30e93c1834b2cf4e2d255fd230c993b2ffcaa
zziadcho <ziad.chourkii@gmail.com>
2024-05-10 03:31:34 +0100

    Added: - opening/reading/closing logic - converting process

---
+++ go-reloaded/app/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	reloaded "reloaded/functions"
+	"strings"
+	//"reloaded/functions"
+)
+
+func main() {
+	// checking argument number
+	if len(os.Args) != 3 {
+		fmt.Println("To run the program use: ./appFile ./inputFile ./outputfile")
+	}
+
+	// assigning file paths
+	inputFile := os.Args[1]
+	// outputFile := os.Args[2]
+
+	// opening input file
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Println("there was an error opening the input file")
+		return
+	}
+	defer file.Close()
+
+	// read input file
+	content, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("there was an error reading the input file")
+		return
+	}
+
+	// file is empty
+	if len(content) == 0 {
+		fmt.Println("the input file is empty")
+		return
+	}
+
+	// converting input file to a string
+	contentString := string(content)
+
+	// split input file to a Split array
+	contentSplit := strings.Split(contentString, " ")
+
+	/* Text Processing Started */
+
+	for i := 0; i <= len(contentSplit); i++ {
+		// the flag is the first element
+		if contentSplit[i] == "(hex)" || contentSplit[i] == "(bin)" || contentSplit[i] == "(up)" || contentSplit[i] == "(low)" || contentSplit[i] == "(cap)" && i == 0 {
+			fmt.Println("the flag cannot be the first element")
+		}
+	}
+	// targeting flags with regexp
+	hexFlag :=
+	binFlag :=
+	upFlag :=
+	lowFlag :=
+	capFlag :=
+	for i, element := range contentSplit{
+
+	}
+
+
+}
