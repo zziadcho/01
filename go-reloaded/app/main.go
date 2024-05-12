879aa9567e9479dcecc3acee8258ad390e2e4cab
zziadcho <ziad.chourkii@gmail.com>
2024-05-12 22:30:52 +0100

    Added logic to detect flags

--- go-reloaded/app/main.go
+++ go-reloaded/app/main.go
@@ -1,61 +1,112 @@
 package main
 
 import (
+	"01/go-reloaded/common/functions"
+	"01/go-reloaded/common/variables"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
-    "01/go-reloaded/common/variables"
 )
 
 func main() {
-	// checking argument number
+	// Checking argument count
 	if len(os.Args) != 3 {
 		fmt.Println("To run the program use: ./appFile ./inputFile ./outputfile")
+		return
 	}
 
-	// assigning file paths
+	// Assigning file paths
 	inputFile := os.Args[1]
-	// outputFile := os.Args[2]
+	outputFile := os.Args[2]
 
-	// opening input file
+	// Opening input file
 	file, err := os.Open(inputFile)
 	if err != nil {
-		fmt.Println("there was an error opening the input file")
+		fmt.Println("Error opening the input file:", err)
 		return
 	}
 	defer file.Close()
 
-	// read input file
+	// Reading input file
 	content, err := io.ReadAll(file)
 	if err != nil {
-		fmt.Println("there was an error reading the input file")
+		fmt.Println("Error reading the input file:", err)
 		return
 	}
 
-	// file is empty
+	// Check if the input file is empty
 	if len(content) == 0 {
-		fmt.Println("the input file is empty")
+		fmt.Println("The input file is empty")
 		return
 	}
 
-	// converting input file to a string
+	// Converting input file to a string and splitting it into an array
 	contentString := string(content)
+	contentSplit :=  []string{}
+	contentSplit = append(contentSplit, contentString)
 
-	// split input file to a Split array
-	contentSplit := strings.Split(contentString, " ")
+	// Text Processing
+	for i, element := range contentSplit {
+		switch {
+		case variables.HexFlag.MatchString(element):
+			convertedWord := strconv.Itoa(functions.ToHex(contentSplit[i-1]))
+			contentSplit[i-1] = convertedWord
 
-	/* Text Processing Started */
+		case variables.BinFlag.MatchString(element):
+			convertedWord := strconv.Itoa(functions.ToBin(contentSplit[i-1]))
+			contentSplit[i-1] = convertedWord
 
-	for i := 0; i <= len(contentSplit); i++ {
-		// the flag is the first element
-		if contentSplit[i] == "(hex)" || contentSplit[i] == "(bin)" || contentSplit[i] == "(up)" || contentSplit[i] == "(low)" || contentSplit[i] == "(cap)" && i == 0 {
-			fmt.Println("the flag cannot be the first element")
+		case variables.UpFlag.MatchString(element):
+			convertedWord := functions.ToUpper(contentSplit[i-1])
+			contentSplit[i-1] = convertedWord
+
+		case variables.UpFlagMulti.MatchString(element):
+			UpFlagNumberMatch := variables.UpFlagMulti.FindStringSubmatch(element)
+			if len(UpFlagNumberMatch) > 1 {
+			  UpFlagNumber, _ := strconv.Atoi(UpFlagNumberMatch[1])
+			  fmt.Println(UpFlagNumber)
+			
+			if i-UpFlagNumber >= 0 && UpFlagNumber > 0 {
+				for j := i - UpFlagNumber; j < i; j++ {
+					contentSplit[j] = functions.ToUpper(contentSplit[j])
+				}
+			}
+		}
+		case variables.LowFlag.MatchString(element):
+			convertedWord := functions.ToLower(contentSplit[i-1])
+			contentSplit[i-1] = convertedWord
+
+		case variables.CapFlag.MatchString(element):
+			convertedWord := functions.Capitalize(contentSplit[i-1])
+			contentSplit[i-1] = convertedWord
 		}
 	}
-	// for i, element := range contentSplit{
-    fmt.Println(variables.HexFlagMulti)
-	// }
 
+	// Finalization
+	contentFinal := []string{}
+	for _, element := range contentSplit {
+		if element != "" {
+			contentFinal = append(contentFinal, element)
+		}
+	}
+	contentRejoin := strings.Join(contentFinal, " ")
 
+	// Creating output file
+	outputFileCreate, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Println("Error creating the output file:", err)
+		return
+	}
+	defer outputFileCreate.Close()
+
+	// Writing into output file
+	_, err = io.WriteString(outputFileCreate, contentRejoin)
+	if err != nil {
+		fmt.Println("Error writing into the output file:", err)
+		return
+	}
+
+	fmt.Print("Processing completed successfully")
 }
