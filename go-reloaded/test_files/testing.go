d9fc06139f2f41638d115fe86e384162402d6c8b
zziadcho <ziad.chourkii@gmail.com>
2024-05-12 22:32:30 +0100

    Relocated files

--- go-reloaded/test_files/testing.go
+++ go-reloaded/test_files/testing.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-    "01/go-reloaded/common/variables"
+	"01/go-reloaded/common/variables"
 	"fmt"
 )
 
-
-func main() { 
-	fmt.Print(variables.HexFlag)
+func main() {
+	text := "it was a (up, 5) beautiful day to begin!"
+	text2 := []string{}
+	text2 = append(text2, text)
+	for _, element := range text2 {
+		if variables.UpFlagMulti.MatchString(element) {
+			fmt.Println("found!")
+			match := variables.UpFlagMulti.FindStringSubmatch(element)
+			if match != nil {
+				numberString := match[1]
+			fmt.Println(numberString)
+		}
+	}
+}
 }
