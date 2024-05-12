740c308de30a965b76d929b54fe2262e6401ec5a
zziadcho <ziad.chourkii@gmail.com>
2024-05-12 22:31:28 +0100

    Reworked regular expression

--- go-reloaded/common/variables/globales.go
+++ go-reloaded/common/variables/globales.go
@@ -4,15 +4,13 @@ import "regexp"
 
 var (
     // targeting flags with regexp
-    HexFlag = regexp.MustCompile(`^\\(hex\\)$`)
-    BinFlag = regexp.MustCompile(`^\\(bin\\)$`)
-    UpFlag = regexp.MustCompile(`^\\(up\\)$`)
-    LowFlag = regexp.MustCompile(`^\\(low\\)$`)
-    CapFlag = regexp.MustCompile(`^\\(cap\\)$`)
+    HexFlag = regexp.MustCompile(`\(hex\)`)
+    BinFlag = regexp.MustCompile(`\(bin\)`)
+    UpFlag = regexp.MustCompile(`\(up\)`)
+    LowFlag = regexp.MustCompile(`\(low\)`)
+    CapFlag = regexp.MustCompile(`\(cap\)`)
     // multi flags
-    HexFlagMulti = regexp.MustCompile(`^\(hex,\s+[0-9]+\)$`)
-    BinFlagMulti = regexp.MustCompile(`^\(bin,\s+[0-9]+\)$`)
-    UpFlagMulti  = regexp.MustCompile(`^\(up,\s+[0-9]+\)$`)
-    LowFlagMulti = regexp.MustCompile(`^\(low,\s+[0-9]+\)$`)
-    CapFlagMulti = regexp.MustCompile(`^\(cap,\s+[0-9]+\)$`)
+    UpFlagMulti  = regexp.MustCompile(`\(up,\s+(\d+)\)`)
+    LowFlagMulti = regexp.MustCompile(`\(low,\s+(\d+)\)`)
+    CapFlagMulti = regexp.MustCompile(`\(cap,\s+(\d+)\)`)
 )
