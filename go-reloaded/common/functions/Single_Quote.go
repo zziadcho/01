19238aa89b96743e1fb2cd7d2fa32582e2461f9b
zziadcho <ziad.chourkii@gmail.com>
2024-05-13 03:55:36 +0100

    Added logic for the function

--- go-reloaded/common/functions/Single_Quote.go
+++ go-reloaded/common/functions/Single_Quote.go
@@ -1,12 +1,22 @@
 package functions
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
-func Regulation(s string) bool {
-	exceptions := "aeuioh"
-	if len(s) > 0 {
-		firstLetter := strings.ToLower(string(s[0]))
-		return strings.ContainsAny(firstLetter, exceptions)
-	}
-	return false
+func handleSingleQuote(text []string) []string {
+	textJoined := strings.Join(text, " ")
+
+	replaceSingleQuotes := regexp.MustCompile(`([a-zA-Z]+)'([a-zA-Z]{1}\s)(\w*)`)
+	textJoined = replaceSingleQuotes.ReplaceAllString(textJoined, "$1\\PLACEHOLDER\\$2$3")
+
+	replaceStandaloneQuotes := regexp.MustCompile(`\s*'\s*(.*?)\s*'\s*`)
+	textJoined = replaceStandaloneQuotes.ReplaceAllString(textJoined, " '$1' ")
+
+	restoreSingleQuotes := regexp.MustCompile(`\\PLACEHOLDER\\`)
+	textJoined = restoreSingleQuotes.ReplaceAllString(textJoined, "'")
+
+	result := strings.Fields(textJoined)
+	return result
 }
