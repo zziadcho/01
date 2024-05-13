a4b19db0a7d4a1539a32986d95264aa21bfece41
zziadcho <ziad.chourkii@gmail.com>
2024-05-13 02:56:45 +0100

    some testing, what did you expect?

--- go-reloaded/test_files/testing.go
+++ go-reloaded/test_files/testing.go
@@ -1,22 +1,36 @@
 package main
 
 import (
-	"01/go-reloaded/common/variables"
 	"fmt"
+	"01/go-reloaded/common/functions"
+	"01/go-reloaded/common/variables"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	text := "it was a (up, 5) beautiful day to begin!"
-	text2 := []string{}
-	text2 = append(text2, text)
-	for _, element := range text2 {
-		if variables.UpFlagMulti.MatchString(element) {
-			fmt.Println("found!")
-			match := variables.UpFlagMulti.FindStringSubmatch(element)
-			if match != nil {
-				numberString := match[1]
-			fmt.Println(numberString)
+	content := "hello guy im gonna show you (up, 9)"
+	contentSplit := strings.Split(content, " ")
+	fmt.Println(len(contentSplit))
+	for i, element := range contentSplit {
+		if variables.UpFlagMulti.MatchString(element) && i+1 < len(contentSplit) {
+			multipiler := contentSplit[i+1]
+			multipiler = strings.TrimRight(multipiler, ")")
+			multipilerInt, err := strconv.Atoi(multipiler)
+			if err != nil {
+				fmt.Println("there was a problem converting the Up multiplier to integer:", err)
+				continue
+			}
+			if multipilerInt <= 0 || i-multipilerInt < 0 {
+				fmt.Println("the Up multiplier is out of range, lower the number to fix the problem:", multipilerInt)
+				continue
+			}
+			for j := i - multipilerInt; j < i; j++ {
+				contentSplit[j] = functions.ToUpper(contentSplit[j])
+			}
+			convertedWord := functions.ToUpper(contentSplit[i-1])
+			contentSplit[i-1] = convertedWord
 		}
 	}
-}
+	fmt.Println(contentSplit)
 }
