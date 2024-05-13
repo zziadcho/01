3884cefc64ae67633b28718e8283558b1cb32a73
zziadcho <ziad.chourkii@gmail.com>
2024-05-13 03:03:02 +0100

    Renamed the function

--- go-reloaded/app/main.go
+++ go-reloaded/app/main.go
@@ -83,23 +83,7 @@ func main() {
 
 			/////////////// multiple up ///////////////
 		case variables.UpFlagMulti.MatchString(element):
-			multipiler := contentSplit[i+1]
-			multipiler = strings.TrimRight(multipiler, ")")
-			multipilerInt, err := strconv.Atoi(multipiler)
-			if err != nil {
-				fmt.Println("there was a problem converting the Up multiplier to integer:", err)
-				continue
-			}
-			if multipilerInt <= 0 || i-multipilerInt < 0 {
-				fmt.Println("the Up multiplier is out of range, lower it to fix the problem")
-				continue
-			}
-			for j := i - multipilerInt; j < i; j++ {
-				contentSplit[j] = functions.ToUpper(contentSplit[j])
-			}
-			convertedWord := functions.ToUpper(contentSplit[i-1])
-			contentSplit[i-1] = convertedWord
-
+			functions.(contentSplit, i, variables.UpFlagMulti, functions.ToUpper, "the Up multiplier is out of range, lower it to fix the problem")
 			/////////////// multiple low ///////////////
 		case variables.LowFlagMulti.MatchString(element):
 			multipiler := contentSplit[i+1]
