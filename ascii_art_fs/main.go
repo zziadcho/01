009d5dc14674a670a4e5234d522eb8d2c8f2e814
zziadcho <ziad.choukrii@gmail.com>
2024-05-23 19:22:28 +0100

    Relocated function

--- ascii_art_fs/main.go
+++ ascii_art_fs/main.go
@@ -7,30 +7,22 @@ import (
 	"strings"
 )
 
-func ParseBannerFile(arg string) bool {
-	if arg == "standard" || arg == "shadow" || arg == "thinkertoy" {
-		return true
-	}
-	return false
-}
-
 // main block
-
 func main() {
 	if len(os.Args) == 3 || len(os.Args) == 2 {
 		var fontFile string
 		if len(os.Args) == 2 {
 			fontFile = functions.ReadFontFile("standard.txt")
 		} else if len(os.Args) == 3 {
-			if ParseBannerFile(os.Args[2]) {
-				fontFile = functions.ReadFontFile(AddTxtExtension(os.Args[2]))
+			if functions.ParseBannerFile(os.Args[2]) {
+				fontFile = functions.ReadFontFile(functions.AddTxtExtension(os.Args[2]))
 			} else {
 				fmt.Println("The Banner File you gave is not right")
 			}
 		}
+		
 		fontParse := functions.ParseFont(fontFile)
 		printableArgs := os.Args[1]
-
 		printableSplit := strings.Split(printableArgs, `\n`)
 
 		for i, line := range printableSplit {
