a686b91df4d711d3b47df4bcd59eaaaba6389674
zziadcho <ziad.choukrii@gmail.com>
2024-05-24 11:09:11 +0100

    New morse code table and translating function

---
+++ myPg/common/functions/MorseCodeInterpeter.go
@@ -0,0 +1,20 @@
+package functions
+
+import (
+	"01/myPg/common/variables"
+	"strings"
+)
+
+func MorseCodeInterpeter(code string) string {
+	var result strings.Builder
+	for _, char := range code {
+		if char == ' '{
+			result.WriteString(" / ") 
+		} else {
+			if morse, found := variables.MorseMap[string(char)]; found {
+				result.WriteString(morse + " ")
+			}
+		}
+	}
+	return result.String()
+}
