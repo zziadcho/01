704b9fa6e8d13ba68fd77dc1a18e6ac6f103545e
zziadcho <ziad.choukrii@gmail.com>
2024-06-07 22:15:09 +0100

    New output newline problem fix

--- ascii-art-output/main.go
+++ ascii-art-output/main.go
@@ -14,14 +14,15 @@ func main() {
 	var outputFile string
 	var fontName string
 	var outputFlag bool
-	
+
 	switch {
+
 	case len(os.Args) == 2:
 		fontFile = functions.ReadFontFile("standard.txt")
 		printableArgs = os.Args[1]
 		fontName = "standard"
 
-	case len(os.Args) == 3 && os.Args[2] == "standard" || os.Args[2] == "shadow" || os.Args[2] == "thinkertoy" :
+	case len(os.Args) == 3 && os.Args[2] == "standard" || os.Args[2] == "shadow" || os.Args[2] == "thinkertoy":
 		fontFile = functions.ReadFontFile(functions.AddTxtExtension(os.Args[2]))
 		printableArgs = os.Args[1]
 		fontName = os.Args[2]
@@ -40,16 +41,17 @@ func main() {
 	fontParse := functions.ParseFont(fontFile, fontName)
 	printableSplit := functions.ArgSplitter(printableArgs)
 	generatedArt := functions.GeneratorLoop(printableSplit, fontParse)
-	switch {
-	case outputFlag && functions.CheckEmpty(printableSplit):
-		os.WriteFile(outputFile, []byte(), 0600)
-		os.Exit(0)
 
-	case functions.CheckEmpty(printableSplit):
+	switch {
+
+	case outputFlag && functions.CheckEmpty(printableSplit, outputFlag, outputFile):
 		os.Exit(0)
 
 	case outputFlag:
 		os.WriteFile(outputFile, []byte(generatedArt), 0600)
+
+	case functions.CheckEmpty(printableSplit, outputFlag, outputFile):
+		os.Exit(0)
 
 	default:
 		fmt.Print(generatedArt)
