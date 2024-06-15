961dc6673a877c79c9d6c801d4841523e9016d1d
zziadcho <ziad.chourkii@gmail.com>
2024-06-15 02:30:30 +0100

    New: - post&get method recognition - ascii-art generator

--- ascii-art-web/main.go
+++ ascii-art-web/main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
+	"01/ascii-art/common/functions"
 	"fmt"
-	"html/template"
+	"log"
 	"net/http"
 )
 
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		log.Println("get")
+	} else if r.Method == http.MethodPost {
+		log.Println("post")
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error", http.StatusBadRequest)
+			return
+		}
+		input := r.Form.Get("input")
+		if input != "" {
+			log.Println("post request with input:", input)
+		}
+		printableSplit := functions.ArgSplitter(input)
+		generatedArt := functions.GeneratorLoop(printableSplit, functions.ParseFont(functions.ReadFontFile("standard.txt"), "standard"))
+		fmt.Println(generatedArt)
+	}
+	http.ServeFile(w, r, "index.html")
+}
 func main() {
-
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("index.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		err = tmpl.Execute(w, "Mehul")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	})
-
-	fmt.Println("server is running on port 8080")
+	http.HandleFunc("/", FormHandler)
+	log.Println("server is on port :8080")
 	http.ListenAndServe(":8080", nil)
 }
