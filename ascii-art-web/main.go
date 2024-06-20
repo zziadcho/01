21c10df0efed6bb051e7285af00e950fcd6294a2
zziadcho <ziad.choukrii@gmail.com>
2024-06-20 19:30:46 +0100

    New logic

--- ascii-art-web/main.go
+++ ascii-art-web/main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
 )
 
-type Data struct{
-	Title string
-	Head string
-	Content string
+type Data struct {
+	Art string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	data := Data{
-		Title: "Welcome",
-		Head: "This is the head",
-		Content: "This is my content",
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		log.Println("getting info")
+		fmt.Println("get")
+	} else if r.Method == http.MethodPost {
+		log.Println("posting info")
+		fmt.Println("post")
 	}
-	tmpl, _ := template.ParseFiles("index.html")
+
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	data := Data{
+		Art: "Ziad",
+	}
 	tmpl.Execute(w, data)
 }
 
-func cssHandler(w http.ResponseWriter, r *http.Request){
-	http.ServeFile(w, r, "style.css")
-}
 func main() {
-	http.HandleFunc("/", handler)
-    http.HandleFunc("/styles.css", cssHandler)
-	log.Println("server is on port :8080")
+	http.HandleFunc("/", FormHandler)
+	fmt.Println("server started on port :8080")
+	fmt.Println("http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
+
 }
