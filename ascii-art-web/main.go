fc9cbc6f7638905c399386acb19fc6105f518b98
zziadcho <ziad.choukrii@gmail.com>
2024-06-27 18:18:11 +0100

    New logic to start server

--- ascii-art-web/main.go
+++ ascii-art-web/main.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"text/template"
+	"01/ascii-art/common/functions"
 )
 
-type Data struct {
-	Art string
-}
-
-func FormHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		log.Println("getting info")
-		fmt.Println("get")
-	} else if r.Method == http.MethodPost {
-		log.Println("posting info")
-		fmt.Println("post")
-	}
-
-	tmpl, err := template.ParseFiles("index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	data := Data{
-		Art: "Ziad",
-	}
-	tmpl.Execute(w, data)
-}
+const PORT = ":8080"
 
 func main() {
-	http.HandleFunc("/", FormHandler)
-	fmt.Println("server started on port :8080")
-	fmt.Println("http://localhost:8080/")
-	http.ListenAndServe(":8080", nil)
-
+	http.HandleFunc("/", functions.MainHandler)
+	http.HandleFunc("/ascii-art-web", functions.HandleAscii)
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
