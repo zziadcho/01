4416769a02955b84e9b5d5199813ae2cf1fc867a
zziadcho <ziad.choukrii@gmail.com>
2024-08-02 00:53:39 +0100

    Rearranged lines

--- ascii-art-web/main.go
+++ ascii-art-web/main.go
@@ -10,11 +10,11 @@ import (
 const PORT = ":8080"
 
 func main() {
+	fmt.Println("http://localhost" + PORT)
 	http.HandleFunc("/", functions.MainHandler)
 	http.HandleFunc("/ascii-art-web", functions.HandleAscii)
 	err := http.ListenAndServe(PORT, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("http://localhost" + PORT)
 }
