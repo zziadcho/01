33c08b17ab17368cf84b026b71c53c723992bf2a
zziadcho <ziad.choukrii@gmail.com>
2024-09-09 02:12:52 +0100

    new Handler for downloading files

--- ascii-art-export-file/common/functions/DownloadHandler.go
+++ ascii-art-export-file/common/functions/DownloadHandler.go
@@ -1,9 +1,30 @@
-package functions 
+package functions
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func DownloadHandler(w http.ResponseWriter, r *http.Request){
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/download" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Disposition", "attachment; filename=download.txt")
+
+	fileContent := UserData.Result
+	size := r.Header.Get("Content-Length")
+	fmt.Println(size)
+	_, err := w.Write([]byte(fileContent))
+	if err != nil {
+		http.Error(w, "Error generating file", http.StatusInternalServerError)
+		return
+	}
 
 }
