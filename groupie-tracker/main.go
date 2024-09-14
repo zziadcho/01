2d2bd28f23fa7f8e980c42dac3fcbad44dd6a08b
zziadcho <ziad.choukrii@gmail.com>
2024-09-14 02:19:06 +0100

    removed old testing structs

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -8,28 +8,12 @@ import (
 
 const PORT = ":8080"
 
-// type Response struct {
-// 	Name    string    `json:"name"`
-// 	Pokemon []Pokemon `json:"pokemon_entries"`
-// }
-
-// type Pokemon struct {
-// 	EntryNo int            `json:"entry_number"`
-// 	Species PokemonSpecies `json:"pokemon_species"`
-// }
-
-// type PokemonSpecies struct {
-// 	Name string `json:"name"`
-// }
 func main() {
-	// Serve static files (CSS, JS, images, etc.)
 	fs := http.FileServer(http.Dir("static"))
+	
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	// Register the main handler
 	http.HandleFunc("/", functions.MainHandler)
 
-	// Start the server
 	log.Println("Server is running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
