5aa864013408371754cf2244aa5a707a4430519d
zziadcho <ziad.choukrii@gmail.com>
2024-09-19 23:26:33 +0100

    new comments, implementation of FetchData()

--- groupie-tracker/common/functions/MainHandler.go
+++ groupie-tracker/common/functions/MainHandler.go
@@ -2,13 +2,12 @@ package functions
 
 import (
 	"01/groupie-tracker/common/utils"
-	"encoding/json"
 	"html/template"
-	"io"
 	"net/http"
 )
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
+	/* protection */
 	if r.URL.Path != "/" {
 		http.Error(w, "403 forbidden", http.StatusForbidden)
 		return
@@ -19,30 +18,21 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	var artists []utils.Artists
+	
+	/* data fetching */
+	err := FetchData("https://groupietrackers.herokuapp.com/api/artists", &artists)
 	if err != nil {
-		http.Error(w, "error getting info from API", http.StatusInternalServerError)
-		return
-	}
-	defer response.Body.Close()
-
-	responseData, err := io.ReadAll(response.Body)
-	if err != nil {
-		http.Error(w, "error reading data", http.StatusInternalServerError)
+		http.Error(w, "500 failed to fetch artists data", http.StatusInternalServerError)
 		return
 	}
 
-	var responseObject []utils.Artists
-	if err := json.Unmarshal(responseData, &responseObject); err != nil {
-		http.Error(w, "error parsing data", http.StatusInternalServerError)
-		return
-	}
-
+	/* template execute */
 	t, err := template.ParseFiles("./common/static/index.html")
 	if err != nil {
-		http.Error(w, "failed to execute template", http.StatusInternalServerError)
+		http.Error(w, "500 failed to execute index template", http.StatusInternalServerError)
 	}
 
-	t.Execute(w, responseObject)
+	t.Execute(w, artists) 
 
 }
