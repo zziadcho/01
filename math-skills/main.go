eb4b765d3b55238b42781160963dc12fb331b9d2
zziadcho <ziad.choukrii@gmail.com>
2024-10-03 12:29:35 +0100

    initial files

--- math-skills/main.go
+++ math-skills/main.go
@@ -1,30 +1,14 @@
 package main
 
 import (
-	// "01/math-skills/common/functions"
+	"01/math-skills/common/functions"
 	"fmt"
-	"io"
-	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Correct Usage: go run . dataFile.txt")
-	}
-	dataFile := os.Args[1]
-
-	file, err := os.Open(dataFile)
-	if err != nil {
-		fmt.Println("Error opening the file", err)
-		return
-	}
-
-	data, err := io.ReadAll(file)
-	if err != nil {
-		fmt.Println("Error reading  the file", err)
-		return
-	}
-
-	fmt.Println(data)
-
+	functions.StartUp()
+	fmt.Println("Average:", functions.Average(functions.Numbers))
+	fmt.Println("Median:", functions.Median(functions.Numbers))
+	fmt.Println("Variance:", functions.Variance(functions.Numbers))
+	fmt.Println("Standard Deviation:", functions.StdDeviation(functions.Numbers))
 }
