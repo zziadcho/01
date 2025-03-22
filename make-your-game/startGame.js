f555e6aa3b920c5017b13872212c3f212d7215ce
zziadcho <ziad.choukrii@gmail.com>
2025-03-22 11:50:26 +0000

    +player damage function +player knockback when damaged

--- make-your-game/startGame.js
+++ make-your-game/startGame.js
@@ -2,14 +2,20 @@
 let playerX = 0, playerY = 0
 let playerWidth = 50, playerHeight = 50
 let playerHP = 10
-const playerSpeed = 10
+let playerSpeed = 10
 
 //enemy stat&info
 let enemyWidth = 75, enemyHeight = 75
 let enemyHP = 3
-const enemySpeed = 5
-const spawnInterval = 2000
-const maxSpawn = 10
+let enemySpeed = 5
+let enemyDamage = 1
+let enemyBuff = 1
+
+//utilities
+let isInvis = false //Invis = invisibility
+let invisDuration = 1500
+const spawnInterval = 200
+const maxSpawn = 1
 const enemies = []
 
 //basic movement
@@ -45,43 +51,78 @@ const startGame = () => {
         }
     }, spawnInterval)
 
-    const gameLoop = () => {
+    //actions
+    const damagePlayer = (damage) => {
+        if (isInvis) return
+        playerHP -= damage
 
-        if (pressedKeys["w"]) playerY -= playerSpeed
-        if (pressedKeys["d"]) playerX += playerSpeed
-        if (pressedKeys["s"]) playerY += playerSpeed
-        if (pressedKeys["a"]) playerX -= playerSpeed
+        isInvis = true
+        setTimeout(() => {
+            isInvis = false
+        }, invisDuration)
+}
+const gameLoop = () => {
+    //keys pressed
+    if (pressedKeys["w"]) playerY -= playerSpeed
+    if (pressedKeys["a"]) playerX -= playerSpeed
+    if (pressedKeys["s"]) playerY += playerSpeed
+    if (pressedKeys["d"]) playerX += playerSpeed
 
-        if (playerX < 0) playerX = 0
-        if (playerY < 0) playerY = 0
-        if (playerX > window.innerWidth - playerWidth) playerX = window.innerWidth - playerWidth
-        if (playerY > window.innerHeight - playerHeight) playerY = window.innerHeight - playerHeight
+    //arena bounderies
+    if (playerX < 0) playerX = 0
+    if (playerY < 0) playerY = 0
+    if (playerX > window.innerWidth - playerWidth) playerX = window.innerWidth - playerWidth
+    if (playerY > window.innerHeight - playerHeight) playerY = window.innerHeight - playerHeight
 
-        enemies.forEach((enemy) => {
-            const xDistance = playerX - enemy.x
-            const yDistance = playerY - enemy.y
-            const distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance)
+    //enemies
+    enemies.forEach((enemy) => {
+        const distanceX = playerX - enemy.x
+        const distanceY = playerY - enemy.y
 
-            if (distance > 0) {
-                enemy.x += (xDistance / distance) * enemySpeed
-                enemy.y += (yDistance / distance) * enemySpeed
+        const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+
+        if (distance > 0) {
+            enemy.x += (distanceX / distance) * enemySpeed
+            enemy.y += (distanceY / distance) * enemySpeed
+        }
+
+        //collision check
+        if (playerX + playerWidth >= enemy.x
+            &&
+            playerX <= enemy.x + enemyWidth
+            &&
+            playerY + playerHeight >= enemy.y
+            &&
+            playerY <= enemy.y + enemyHeight
+        ) {
+            damagePlayer(enemyDamage)
+            const absX = Math.abs(distanceX)
+            const absY = Math.abs(distanceY)
+
+            if (absX > absY) {
+                if (distanceX < 0) {
+                    playerX -= 75
+                } else {
+                    playerX += 75
+                }
+            } else {
+                if (distanceY < 0) {
+                    playerY -= 75
+                } else {
+                    playerY += 75
+                }
             }
+        }
 
-            enemy.element.style.left = `${enemy.x}px`
-            enemy.element.style.top = `${enemy.y}px`
+        enemy.element.style.left = `${enemy.x}px`
+        enemy.element.style.top = `${enemy.y}px`
 
-            if (playerX < enemy.x + enemyWidth &&
-                playerX + playerWidth > enemy.x &&
-                playerY < enemy.y + enemyHeight &&
-                playerY + playerHeight > enemy.y) {
-                console.log("collided")
-            }
-        });
+    })
+    //player
+    player.style.left = `${playerX}px`
+    player.style.top = `${playerY}px`
 
-        player.style.left = `${playerX}px`
-        player.style.top = `${playerY}px`
-
-        requestAnimationFrame(gameLoop)
-    }
     requestAnimationFrame(gameLoop)
+}
+requestAnimationFrame(gameLoop)
 }
